601bce07922909e30f95d91338f06df180ce5134
COZZY20 <ongzz-wp20@student.tarc.edu.my>
2023-05-09 19:20:34 +0800

    Liquid Wobble and Thank You scene

---
+++ ARChemistry/Assets/Script/SequenceAnimator.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceAnimator : MonoBehaviour
+{
+    public float WaitBetween = 0.15f;
+    public float WaitEnd = 0.5f;
+
+    List<Animator> animators;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animators = new List<Animator>(GetComponentsInChildren<Animator>());
+
+        StartCoroutine(DoAnimation());
+    }
+
+    IEnumerator DoAnimation()
+    {
+        while (true)
+        {
+            foreach (var animator in animators)
+            {
+                animator.SetTrigger("DoAnimation");
+                yield return new WaitForSeconds(WaitBetween);
+            }
+
+            yield return new WaitForSeconds(WaitEnd);
+        }
+    }
+}
